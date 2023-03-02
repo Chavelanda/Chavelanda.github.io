5ef634d3186f59097fc3a1bda7500404a84490ca
Giacomo Melacini <gmelacini@unibz.it>
2023-03-02 17:55:11 +0100

    Fixed mobile header

--- docs/script/shrink_header_on_scroll.js
+++ docs/script/shrink_header_on_scroll.js
@@ -7,8 +7,8 @@ function scrollFunction() {
     if (x.matches) {
       document.getElementById("header_1").style.gridColumn = "2 / span 3";
       document.getElementById("header_2").style.display = "none";
-      document.getElementById("about_h").style.gridColumn = "6";
-      document.getElementById("projects_h").style.gridColumn = "8";
+      document.getElementById("about_h").style.gridColumn = "8";
+      document.getElementById("projects_h").style.gridColumn = "9";
       document.getElementById("contacts_h").style.gridColumn = "10";
       } else {
       document.getElementById("header_1").style.gridColumn = "1 / span 3";
@@ -19,8 +19,8 @@ function scrollFunction() {
     }
 
     document.getElementById("header").style.height = "100px";
-
     document.getElementById("header_4").style.display = "none";
+    document.getElementById("header_5").style.display = "none";
 
     document.getElementById("project-name").style.fontSize = "large";
     document.getElementById("project-tagline").style.display = "none";
@@ -36,7 +36,8 @@ function scrollFunction() {
     document.getElementById("about_h").style.gridColumn = "5";
     document.getElementById("projects_h").style.gridColumn = "6";
     document.getElementById("contacts_h").style.gridColumn = "7";
-    document.getElementById("header_4").style.display = "block";
+    document.getElementById("header_4").style.display = "revert";
+    document.getElementById("header_5").style.display = "revert";
 
     document.getElementById("project-name").style.fontSize = "xx-large";
     document.getElementById("project-tagline").style.display = "revert";
